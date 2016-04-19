b057319a532f85f8ab6d5beeedc65aef2720f07d
GillesC <callebaut.gilles@gmail.com>
2016-04-19 13:54:02 +0200

    Added gaussian

---
+++ src/main/java/DecisionMaker/DecisionMaker.java
@@ -0,0 +1,7 @@
+package DecisionMaker;
+
+/**
+ * Created by Gilles Callebaut on 19/04/2016.
+ */
+public class DecisionMaker {
+}
--- src/main/java/Tools/RNG.java
+++ src/main/java/Tools/RNG.java
@@ -4,18 +4,18 @@ import java.util.Random;
 
 /**
  * Created by Lukas on 19-Apr-16.
+ *
  */
 public class RNG {
     private static RNG rng = new RNG();
+    private Random random = new Random();
+    private double standardDev;
+    private double mean;
+    private boolean initGaussion;
+
     public static synchronized RNG getInstance() {
         return rng;
     }
-
-
-
-    private Random random;
-
-
 
     public synchronized Random getRandom() {
         return random;
@@ -29,5 +29,15 @@ public class RNG {
         return random.nextInt(bound);
     }
 
+    public synchronized void initGuassion(double desiredStandardDeviation, double desiredMean) {
+        this.initGaussion = true;
+        this.standardDev = desiredStandardDeviation;
+        this.mean = desiredMean;
+    }
+
+    public synchronized double nexGaussion() throws Exception {
+        if(!initGaussion) throw new Exception("Call initGaussion first!");
+        return random.nextGaussian()*standardDev+mean;
+    }
 
 }
