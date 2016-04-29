fe1be3c5851ce835b54af247896848a8487b8640
Lukas Vanhoucke <vanhouckelukas@gmail.com>
2016-04-29 10:58:32 +0200

    Pool constructor changes

--- src/main/java/GUI/GUIController.java
+++ src/main/java/GUI/GUIController.java
@@ -143,7 +143,7 @@ public class GUIController {
 
 
 
-                PurchaseThreadPool pool = new PurchaseThreadPool(ag.generateAgents(), pg.generateProducts(), cps);
+                PurchaseThreadPool pool = new PurchaseThreadPool(agents, products, cps);
                 pool.run();
 
             }
